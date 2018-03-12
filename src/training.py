734b562dee80f914d4f546336fecdcd6e126f4d6
Yongki Jay An <hinza0188@gmail.com>
2018-03-12 18:14:40 -0400

    added frames for the functions

---
+++ src/training.py
@@ -0,0 +1,21 @@
+import numpy as np
+from read_data import load_data
+from sklearn import neighbors
+import matplotlib.pyplot as plt
+
+n_neighbors = 15
+data = load_data()
+
+
+def training():
+    pass
+    #clf = neighbors.KNeighborsClassifier(n_neighbors)
+    #clf.fit(X, y)
+
+
+def main():
+    training()
+
+
+if __name__ == "__main__":
+    main()
