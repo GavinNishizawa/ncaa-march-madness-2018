a31d8ba5dc5ebcdb14cb42fee78f2d29fd17574c
Gavin Nishizawa <ghn6069@rit.edu>
2018-03-12 14:36:16 -0400

    Pickle data for faster loading

--- src/read_data.py
+++ src/read_data.py
@@ -4,9 +4,20 @@ Read in the data from csv files
 
 import pandas as pd
 import numpy as np
+import os
+import pickle
 
 def load_csv(filename):
-    return pd.read_csv(filename)
+    pickle_fn = filename+".pkl"
+
+    # load data from pickle file if it exists
+    if os.path.isfile(pickle_fn):
+        return pickle.load(open(pickle_fn, 'rb'))
+    # otherwise load from csv
+    else:
+        data = pd.read_csv(filename)
+        pickle.dump(data, open(pickle_fn, 'wb'))
+        return data
 
 
 def main():
