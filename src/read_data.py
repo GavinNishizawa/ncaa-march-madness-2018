ee01e0d04082a13c4f9b25166dea43a3d881d161
Gavin Nishizawa <ghn6069@rit.edu>
2018-03-12 15:58:08 -0400

    Load data from all csv files

--- src/read_data.py
+++ src/read_data.py
@@ -5,7 +5,10 @@ Read in the data from csv files
 import pandas as pd
 import numpy as np
 import os
+import csv
+import glob
 import pickle
+
 
 def load_csv(filename):
     pickle_fn = filename+".pkl"
@@ -13,17 +16,48 @@ def load_csv(filename):
     # load data from pickle file if it exists
     if os.path.isfile(pickle_fn):
         return pickle.load(open(pickle_fn, 'rb'))
+
     # otherwise load from csv
     else:
-        data = pd.read_csv(filename)
+        data = pd.read_csv(filename, encoding="latin_1")
         pickle.dump(data, open(pickle_fn, 'wb'))
         return data
 
 
-def main():
+def load_data():
+    pickle_fn = "data/loaded_data.pkl"
+
+    # load data from pickle file if it exists
+    if os.path.isfile(pickle_fn):
+        return pickle.load(open(pickle_fn, 'rb'))
+
+    # otherwise load from csv
+    else:
+        df_dict = {}
+
+        # load all csv files in data directory
+        for f in glob.glob(os.path.join("data","*.csv")):
+            # key based on their filename
+            f_key = f.split('/')[1].split('.')[0]
+
+            print("Loading:",f_key)
+            df_dict[f_key] = load_csv(f)
+
+        pickle.dump(df_dict, open(pickle_fn, 'wb'))
+        return df_dict
+
+
+def test_load_csv():
     print("Test loading csv")
     data = load_csv("data/NCAATourneySeeds.csv")
     print(data)
+
+
+def main():
+    print("Test loading data")
+    data = load_data()
+    print(data)
+
 
 if __name__=="__main__":
     main()
