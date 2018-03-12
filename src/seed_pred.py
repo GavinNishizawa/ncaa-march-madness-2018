3a5b0d32c0d36f77ae3c20272c6b20b02a4779d1
Gavin Nishizawa <ghn6069@rit.edu>
2018-03-12 18:20:53 -0400

    Fix seed to number conversion

--- src/seed_pred.py
+++ src/seed_pred.py
@@ -6,11 +6,6 @@ from read_data import load_data
 data = load_data()
 def main():
     data = load_data()
-    #print(data["Seasons"].plot())
-    #plt.show()
-    #print(data["Seasons"]["RegionZ"])
-    #data["RegularSeasonCompactResults"]["WTeamID"]
-    #data["RegularSeasonCompactResults"]["LTeamID"]
     id_to_region(data,1385,1985)
 
 def id_to_region(data, id, year):
@@ -23,7 +18,8 @@ def id_to_region(data, id, year):
     first_letter = currSeed[:1]
     seednumber = int(currSeed[1:3])
 
-    return x.index(first_letter)*16 + seednumber
+    # the magic formula to convert seed to a numerical value
+    return x.index(first_letter) + 1 + (seednumber-1)*4
 
 if __name__=="__main__":
     main()
