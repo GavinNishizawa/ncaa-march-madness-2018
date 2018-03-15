5dfbb5ab0f11980812873b6841bfbe39377d872b
Gavin Nishizawa <ghn6069@rit.edu>
2018-03-15 07:06:40 -0400

    Make predictions file

--- src/make_predictions.py
+++ src/make_predictions.py
@@ -1,7 +1,10 @@
+import os
+import itertools as itr
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from read_data import load_data
+from save_data import save_object, load_object
 from run_model import apply_pca, run_prediction
 from seed_pred import get_seed_data
 
@@ -18,35 +21,69 @@ def seed_val(seed):
 def get_test_data(data):
     seedData = data["NCAATourneySeeds"]
     seedData2018 = seedData[seedData["Season"] == 2018]
+    print(seedData2018)
+    sD2018 = pd.DataFrame(list(itr.combinations( seedData2018.TeamID, 2)))
+    sD2018.columns = ["TeamID_l","TeamID_r"]
+    sD2018["TeamID_x"] = sD2018.apply(
+            lambda r: int(r["TeamID_l"]) \
+                if int(r["TeamID_l"]) < int(r["TeamID_r"]) \
+                else int(r["TeamID_r"]), axis=1)
+    sD2018["TeamID_y"] = sD2018.apply(
+            lambda r: int(r["TeamID_r"]) \
+                if int(r["TeamID_l"]) < int(r["TeamID_r"]) \
+                else int(r["TeamID_l"]), axis=1)
+    sD2018 = sD2018[["TeamID_x","TeamID_y"]]
+    sD2018 = sD2018.sort_values(["TeamID_x","TeamID_y"])
+    print(sD2018)
+    sD2018 = sD2018.merge(seedData2018, left_on=["TeamID_x"], right_on=["TeamID"])
+    sD2018 = sD2018[["TeamID_x","TeamID_y","Season","Seed"]]
+    print("sD:",sD2018)
+    print("seed:",seedData2018)
+    sD2018 = sD2018.merge(seedData2018,\
+            left_on=["TeamID_y","Season"],\
+            right_on=["TeamID","Season"])
+    print(sD2018)
+    sD2018 = sD2018[["TeamID_x","TeamID_y",
+        "Season","Seed_x","Seed_y"]]
+
+    # convert seeds to seed values
+    sD2018["Seed_xv"] = sD2018["Seed_x"].apply(seed_val)
+    sD2018["Seed_yv"] = sD2018["Seed_y"].apply(seed_val)
+
+
     seasonResults = data["AllCompactResults"]
 
+    '''
     # join NCAATourneySeeds on CompactResults where TeamID=WTeamID and Season=Season
     r_data = seedData.merge(seasonResults, left_on=["TeamID","Season"], right_on=["WTeamID","Season"])
 
     # join r_data on NCAATourneySeeds where LTeamID=TeamID and Season=Season
     r_data = r_data.merge(seedData, left_on=["LTeamID","Season"], right_on=["TeamID","Season"])
+    '''
 
-    # convert seeds to seed values
-    r_data["Seed_xv"] = r_data["Seed_x"].apply(seed_val)
-    r_data["Seed_yv"] = r_data["Seed_y"].apply(seed_val)
+    r_data = seasonResults
     r_data["WLoc"] = r_data["WLoc"].apply(
         lambda x: 1 if x == 'H' else 0)
-    r_data["Avg_WLoc_x"] = r_data["TeamID_x"].apply(
+    print("="*20,"r_data")
+    print("="*20,"r_data")
+    print(r_data)
+    r_data["Avg_WLoc_x"] = r_data["WTeamID"].apply(
             lambda x:
-            r_data[r_data["TeamID_x"] == x]["WLoc"].mean())
-    r_data["Avg_WLoc_y"] = r_data["TeamID_y"].apply(
+            r_data[r_data["WTeamID"] == x]["WLoc"].mean())
+    r_data["Avg_WLoc_y"] = r_data["LTeamID"].apply(
             lambda x:
-            r_data[r_data["TeamID_y"] == x]["WLoc"].mean())
-    r_data["Avg_score_x"] = r_data["TeamID_x"].apply(
+            r_data[r_data["WTeamID"] == x]["WLoc"].mean())
+    r_data["Avg_score_x"] = r_data["WTeamID"].apply(
             lambda x:
-            r_data[r_data["TeamID_x"] == x]["WScore"].mean())
-    r_data["Avg_score_y"] = r_data["TeamID_y"].apply(
+            r_data[r_data["WTeamID"] == x]["WScore"].mean())
+    r_data["Avg_score_y"] = r_data["LTeamID"].apply(
             lambda x:
-            r_data[r_data["TeamID_y"] == x]["LScore"].mean())
+            r_data[r_data["WTeamID"] == x]["WScore"].mean())
+    print(r_data)
 
 
-    r_data = r_data.merge(seedData2018, left_on=["Seed_x","TeamID_x"], right_on=["Seed","TeamID"])
-
+    sD2018 = sD2018.merge(r_data, left_on=["TeamID_x","TeamID_y"], right_on=["WTeamID","LTeamID"])
+    print(sD2018)
 
     # trim to wanted values
     r_data = r_data[[
@@ -71,20 +108,35 @@ def main():
     data = load_data()
     seed_data = get_seed_data(data)
     train_data = seed_data.values
-    print(train_data.shape)
 
-    test_data = get_test_data(data).values
-    print(test_data.shape)
+    td_fn = os.path.join("data","test_predict_data")
+    # load test_data if it exists
+    test_data = load_object(td_fn)
+
+    if test_data == None:
+        test_data = get_test_data(data)
+        save_object(td_fn, test_data)
 
     t_data = {
         "train_target": train_data[:, -1],
         "train_data": train_data[:, :-1],
-        "test_data": test_data
+        "test_data": test_data.values
         }
 
     t_data = apply_pca(t_data)
     ps = run_prediction("knn", t_data)
-    print(ps)
+    ps = np.array(ps)
+    print(ps.shape)
+    data_2018["prediction"] = pd.DataFrame(ps[:,0])
+    data_2018["id"] = data_2018.apply(
+            lambda r: "2018_"+
+                str(int(r["TeamID_x"]))+"_"+
+                str(int(r["TeamID_y"])), axis=1)
+
+    data_2018 = data_2018[["id","prediction"]]
+
+    print(data_2018)
+    data_2018.to_csv("predictions_2018.csv", index=False)
 
 
 if __name__=="__main__":
